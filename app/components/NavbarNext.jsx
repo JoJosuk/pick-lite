f909c1e090e34222e1b1b563f2b1297130fd8a43
Joel J George <joeljoby111@gmail.com>
2024-02-04 08:17:54 +0530

    feat: added the searchicon

--- app/components/NavbarNext.jsx
+++ app/components/NavbarNext.jsx
@@ -5,6 +5,7 @@ import {
   NavbarContent,
   NavbarItem,
   Link,
+  Input,
   DropdownItem,
   DropdownTrigger,
   Dropdown,
@@ -12,9 +13,10 @@ import {
   Avatar,
 } from "@nextui-org/react";
 import PickliteLogo from "./PickliteLogo";
+import SearchIcon from "./SearchIcon";
 export default function NavbarNext() {
   return (
-    <Navbar>
+    <Navbar className="pb-2">
       <NavbarBrand>
         <PickliteLogo />
 
@@ -27,6 +29,19 @@ export default function NavbarNext() {
       ></NavbarContent>
 
       <NavbarContent as="div" justify="end">
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper:
+              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<SearchIcon size={18} />}
+          type="search"
+        />
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
             <Avatar
