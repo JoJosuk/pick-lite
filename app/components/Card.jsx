ff526ee6ae926e1bd2b08168f6819dd6cbf64772
Joel J George <joeljoby111@gmail.com>
2024-02-11 23:59:10 +0530

    fix(dashboard): fixed  background

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import { Chip, Link } from "@nextui-org/react";
-export default function Card() {
-  const tags = ["React", "Next.js", "Tailwind CSS"];
+export default function Card({ id, title, description, tags, link }) {
   return (
     <div
       href="#"
-      className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      className="flex flex-col min-w-unit-8xl items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
       <img
         className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
@@ -13,13 +12,12 @@ export default function Card() {
         alt="hey not working image"
       />
 
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-[#F59E0B]">
-          Noteworthy technology acquisitions 2021
+      <div className="flex flex-col justify-between p-4 leading-normal ">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-[#F59E0B] ">
+          {title}
         </h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          {description}
         </p>
         <div className="flex flex-wrap gap-1">
           {tags.map((tag, index) => (
@@ -33,7 +31,7 @@ export default function Card() {
           color="warning"
           isExternal
           showAnchorIcon
-          href="https://github.com/nextui-org/nextui"
+          href={link}
           // anchorIcon={<AnchorIcon />}
         >
           Custom Icon
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import NavbarNext from "../components/NavbarNext";
 import Tagbar from "../components/Tagbar";
+import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Card from "../components/Card";
@@ -23,7 +24,11 @@ export default function Dashboard() {
   useEffect(() => {
     const functionCollection = async () => {
       const checkUserValid = await validateUser();
-      if (!checkUserValid) router.push("/");
+      console.log("check user valid ", checkUserValid);
+      if (!checkUserValid) {
+        console.log("user not valid");
+        router.push("/");
+      }
       const resultListValues = await fetchData();
       console.log("result is list ", resultListValues);
       setResultList(resultListValues);
@@ -37,17 +42,45 @@ export default function Dashboard() {
 
   useEffect(() => {}, [searchValue]);
   return (
-    <div>
+    <div className=" w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className=" w-screen min-h-screen antialiased">
         <NavbarNext onSearchInputChange={handleSearchInput} />
         <Tagbar tags={tags} onTagSelect={setTags} />
         <div className="flex z-50 flex-col gap-3 items-center">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {resultList.map((item, index) => (
+            <Card
+              key={index}
+              id={item.id}
+              title={item.title}
+              description={item.description}
+              tags={item.tags}
+              link={item.link}
+            />
+            // <div key={index}>
+            //   <Card
+            //     id={item.id}
+            //     title={item.title}
+            //     description={item.description}
+            //     tags={item.tags}
+            //     link={item.link}
+            //   />
+            //   <Card
+            //     id={item.id}
+            //     title={item.title}
+            //     description={item.description}
+            //     tags={item.tags}
+            //     link={item.link}
+            //   />
+            //   <Card
+            //     id={item.id}
+            //     title={item.title}
+            //     description={item.description}
+            //     tags={item.tags}
+            //     link={item.link}
+            //   />
+            // </div>
+          ))}
         </div>
       </div>
     </div>
