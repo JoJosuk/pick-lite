ac42fa69d04d45c43df7963c80593387fb3b1243
Joel J George <joeljoby111@gmail.com>
2024-02-21 23:47:57 +0530

    fix(otp): used the memcachier for deployment in vercel

--- app/api/otp/route.js
+++ app/api/otp/route.js
@@ -5,33 +5,70 @@ import prisma from "@lib/prisma";
 const jwt = require("jsonwebtoken");
 import { cookies } from "next/headers";
 const resend = new Resend(process.env.RESENDAPIKEY);
+const memjs = require("memjs");
 import mailContentGenerator from "app/components/functions/emailmarkdown";
-const otpDict = new NodeCache();
+const otpCache = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+  failover: true,
+  timeout: 1,
+  keepAlive: true,
+});
 export async function POST(request) {
   const { email } = await request.json();
   const username = email.split("@")[0];
-  const otp = otpVal();
-  if (otpDict.set(email, otp, 320)) {
-    console.log(otpDict.get(email));
+  otpCache.set(email, otpVal(), { expires: 300 }, function (err, val) {
+    if (err) {
+      console.log("error setting otp value", err);
+    }
+  });
+  const otp = otpCache.get(email, function (err, val) {
+    if (err) {
+      console.log("error getting otp value", err);
+    }
+    console.log("otp value and email while creating", val.toString(), email);
+    return val.toString();
+  });
+  // console.log("otpval", otp);
+
+  if (otp) {
+    const resultString = mailContentGenerator(otp);
+
+    try {
+      resend.emails.send({
+        from: "dev@joeljgeorge.tech",
+        to: "joeljoby111@gmail.com",
+        subject: `Hello ${username}`,
+        html: resultString,
+      });
+    } catch (e) {
+      console.log("error while sending mail", e);
+    }
+    console.log("email sent");
+    return NextResponse.json({ status: "ok" });
+  } else {
+    return NextResponse.json({ status: "fail" });
   }
-  const resultString = mailContentGenerator(otp);
-  try {
-    resend.emails.send({
-      from: "dev@joeljgeorge.tech",
-      to: "joeljoby111@gmail.com",
-      subject: `Hello ${username}`,
-      html: resultString,
-    });
-  } catch (e) {
-    console.log("error while sending mail", e);
-  }
-  console.log("email sent");
-  return NextResponse.json({ status: "ok" });
 }
 export async function PUT(request, response) {
   const { email, otp } = await request.json();
-  console.log(otpDict.get(email));
-  if (otpDict.get(email) == otp) {
+  // console.log("email and otp", email, otp);
+  const otpValue = await new Promise((resolve, reject) => {
+    otpCache.get(email, function (err, val) {
+      if (err) {
+        console.log("error getting otp value", err);
+        reject(err);
+      } else {
+        console.log(
+          "otp value and email while verifying",
+          val.toString(),
+          email
+        );
+        resolve(val.toString());
+      }
+    });
+  });
+
+  console.log("otpval in verification", otpValue);
+  if (otpValue == otp) {
     const user = await prisma.user.findFirst({
       where: {
         email: email,
